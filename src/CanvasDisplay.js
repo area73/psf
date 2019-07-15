c059d812362335bc35b3e64d0198b4b79421b908
rerades <rerades@siete3.com>
2019-07-15 19:06:39 +0200

    WIP canvas display

--- src/CanvasDisplay.js
+++ src/CanvasDisplay.js
@@ -1,19 +1,16 @@
 // Display
-export const CanvasDisplay = ({
-  doc,
-  id,
-  width,
-  height
-} = {
-  doc:document,
-  id:'canvas',
-  width: 300,
-  height:300,
-})  => ({
+export const CanvasDisplay = (
+  { doc, id, width, height } = {
+    doc: document,
+    id: 'canvas',
+    width: 300,
+    height: 300,
+  },
+) => ({
   doc,
   canvas: () => this.doc.getElementById(id),
   ctx: () => this.canvas.getContext('2d'),
   width,
   height,
-  id
+  id,
 });
